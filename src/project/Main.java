e85cfaa70b8fdaf7a5e3586b51c24f4924e9ce15
Kate Pestrikova <volkva2@gmail.com>
2020-09-06 12:07:22 +0500

    added all files

---
+++ src/project/Main.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Main {
+    public static void main(String[] args) {
+        int account;
+        account = 2_000_000_000;
+        int transfer;
+        transfer = 500_000_000;
+        int total;
+        total = account + transfer;
+        System.out.println(total);
+    }
+}
